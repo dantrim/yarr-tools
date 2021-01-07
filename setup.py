09a91efaca2731a3ace330a05269afe9c35cd6b3
dantrim <dantrim1023@gmail.com>
2021-01-07 10:34:02 -0800

    ci and formatting

---
+++ setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# the directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# the text of the README file
+README = (HERE / "README.rst").read_text()
+
+setup(
+    name="yarr-tools",
+    version="1.0.0",
+    description="Utilities for interacting with the YARR DAQ software",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/dantrim/yarr-tools",
+    author="Daniel Joseph Antrim",
+    author_email="dantrim1023@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    install_requires=["numpy", "click", "matplotlib"],
+    entry_points={"console_scripts": []},
+)
